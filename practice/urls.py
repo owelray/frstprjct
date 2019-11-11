1fc68702929243b33f8a2260031915ecb3efefe5
owelray <57111549+owelray@users.noreply.github.com>
2019-11-11 21:50:04 +0200

    Started working on the second_project

--- practice/urls.py
+++ practice/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path, path
+from django.urls import path
 from django.urls.conf import  include
 
 
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
     path('first_project/', include('first_project.urls')),
+    path('second_project/', include('second_project.urls')),
 ]
