1aa733598a0a119302c391bbac020b126e5699a0
owelray <57111549+owelray@users.noreply.github.com>
2019-11-21 21:00:19 +0200

    Created a list of reviews in the main page.

--- second_project/models.py
+++ second_project/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=60)
     author = models.CharField(max_length=35)
     review = models.TextField(max_length=250)
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, default="1")
--- second_project/urls.py
+++ second_project/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
         path('login/', views.LoginFormView.as_view()),
         path('', views.main),
         path('logout/', views.LogoutView.as_view()),
+        path('add/logout/', views.LogoutView.as_view()),
         path('add/', views.add),
         path('add/create/', views.create)
 ]
