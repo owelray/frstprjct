96f612d383e320cc58f85b1516f94a02eaa3188f
owelray <57111549+owelray@users.noreply.github.com>
2019-12-28 16:41:36 +0200

    Removed first project

--- first_project/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FirstProjectConfig(AppConfig):
-    name = 'first_project'
--- first_project/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Person(models.Model):
-    name = models.CharField(max_length=20, default="Unknown")
-    age = models.IntegerField()
-
