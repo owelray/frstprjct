3919ec999c6104aa1d34c87a580d44d1fedf6e5e
owelray <57111549+owelray@users.noreply.github.com>
2020-03-19 18:11:01 +0200

    Configured out sessions and fixed the possibility of creating identical hashes

--- second_project/models.py
+++ second_project/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class Url(models.Model):
     long_url = models.URLField()
+    short_url = models.URLField()
     use_bitly = models.BooleanField(default=False)
-    short_url = models.CharField(max_length=32)
+    url_hash = models.CharField(max_length=32, default=0)
+    creator = models.CharField(max_length=32, default='Unknown')
     clicks_counter = models.IntegerField(default=0)
+    unique_visitors = models.IntegerField(default=0)
--- second_project/views.py
+++ second_project/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .forms import UrlForm
 from django.views.generic.base import View
 from .models import Url
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpRequest
 import hashlib
+
+HOST = 'localhost:8000/second_project/'
 
 class MainView(View):
     def post(self, request):
@@ -11,19 +13,29 @@ class MainView(View):
             form = UrlForm(request.POST)
             if form.is_valid():
                 url = Url()
+                url.creator = request.session.session_key
                 url.long_url = request.POST.get('url')
                 url.use_bitly = request.POST.get('shortening_method')
                 if url.use_bitly == None:
                     url.use_bitly = False
                 if url.use_bitly == "on":
                     url.use_bitly = True
-                print(url.use_bitly)
-                url.short_url = hashlib.md5(url.long_url.encode())
-                url.short_url = url.short_url.hexdigest()[:8]
+                url.url_hash = hashlib.md5(url.long_url.encode())
+                url.url_hash = url.url_hash.hexdigest()[:8]
+                url_hash_exists = Url.objects.filter(url_hash=url.url_hash)
+                while url_hash_exists:
+                    url.url_hash = hashlib.md5(url.url_hash.encode())
+                    url.url_hash = url.url_hash.hexdigest()[:8]
+                    url_hash_exists = Url.objects.filter(url_hash=url.url_hash)
+                    continue
+                url.short_url = HOST + url.url_hash
                 url.save()
                 return HttpResponseRedirect('/second_project')
             else:
                 return HttpResponseRedirect('/')
     def get(self, request):
+        request.session.save()
+        current_user = request.session.session_key
+        users_urls = Url.objects.filter(creator=current_user)
         form = UrlForm()
-        return render(request, 'second_project/url_shortener.html', {'form': form})
+        return render(request, 'second_project/url_shortener.html', {'form': form, 'user': users_urls})
