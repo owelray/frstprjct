5a5530c4ca0de8984c573a5ad349b50abd3a499f
owelray <57111549+owelray@users.noreply.github.com>
2020-03-25 14:43:45 +0200

    Updated paths

--- second_project/urls.py
+++ second_project/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-        path('', views.MainView.as_view())
+        path('', views.MainView.as_view()),
+        path('<hash>', views.RedirectView.as_view()),
+        path('clear/<hash>', views.ClearStatsView.as_view()),
+        path('delete/<hash>', views.DeleteUrlView.as_view())
 ]
