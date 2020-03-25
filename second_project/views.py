ed72ecf718f2bb02a17bb51fe75da8c8b7093225
owelray <57111549+owelray@users.noreply.github.com>
2020-03-25 14:35:16 +0200

    Added redirect action and possibility to clear stats and delete url

--- second_project/views.py
+++ second_project/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .forms import UrlForm
 from django.views.generic.base import View
-from .models import Url
-from django.http import HttpResponseRedirect, HttpRequest
+from .models import Url, Visitor
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect, HttpRequest, HttpResponseNotFound
 import hashlib
 
 HOST = 'localhost:8000/second_project/'
@@ -32,10 +33,63 @@ class MainView(View):
                 url.save()
                 return HttpResponseRedirect('/second_project')
             else:
-                return HttpResponseRedirect('/')
+                return HttpResponseRedirect('/second_project/nt')
+
     def get(self, request):
         request.session.save()
         current_user = request.session.session_key
         users_urls = Url.objects.filter(creator=current_user)
         form = UrlForm()
         return render(request, 'second_project/url_shortener.html', {'form': form, 'user': users_urls})
+
+class RedirectView(View):
+        def get(self, request, hash):
+            try:
+                visitor = Visitor()
+                request.session.save()
+                current_session_key = request.session.session_key
+                url = Url.objects.get(url_hash=hash)
+                visitor.url_id = url.id
+                visitor_exist = Visitor.objects.filter(session_key=current_session_key, url_id=url.id)
+                if visitor_exist:
+                    pass
+                else:
+                    visitor.session_key = current_session_key
+                    visitor.save()
+                    url.unique_visitors += 1
+                url.clicks_counter += 1
+                url.save()
+                url = url.long_url
+                return HttpResponseRedirect(url)
+            except Url.DoesNotExist:
+                return HttpResponseNotFound('<h2>Url not found</h2>')
+
+class ClearStatsView(View):
+    def get(self, request, hash):
+        try:
+            current_session_key = request.session.session_key
+            url = Url.objects.get(url_hash=hash)
+            if current_session_key == url.creator:
+                url.clicks_counter = 0
+                url.unique_visitors = 0
+                url.save()
+                return HttpResponseRedirect('/second_project')
+            else:
+                return HttpResponseRedirect('/second_project/nt')
+        except Url.DoesNotExist:
+            return HttpResponseNotFound('<h2>Url not found</h2>')
+
+class DeleteUrlView(View):
+    def get(self, request, hash):
+        try:
+            current_session_key = request.session.session_key
+            url = Url.objects.get(url_hash=hash)
+            if current_session_key == url.creator:
+                url.delete()
+                return HttpResponseRedirect('/second_project')
+            else:
+                return HttpResponseRedirect('/second_project/nt')
+        except Url.DoesNotExist:
+            return HttpResponseNotFound('<h2>Url not found</h2>')
+
+
