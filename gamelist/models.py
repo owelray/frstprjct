21590689658eee9f4498178c04026c7d1b6acdfb
Owel <owelray@gmail.com>
2020-04-16 17:40:56 +0300

    Created a gamereview model

--- gamelist/models.py
+++ gamelist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class GameReview(models.Model):
+    game_title = models.CharField(max_length=128)
+    review = models.CharField(max_length=450)
+    reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.FloatField(max_length=10, default=0)
+    likenumber = models.IntegerField(default=0)
+    likedone = models.ManyToManyField(User, related_name='users_likes')
+    date = models.DateTimeField(auto_now_add=True)
