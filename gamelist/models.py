20b7b8cda53e7aa9bb76b64e34348b2956104ca5
Owel <owelray@gmail.com>
2020-04-19 17:08:24 +0300

    null == true to rating

--- gamelist/models.py
+++ gamelist/models.py
@@ -3,10 +3,10 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 class GameReview(models.Model):
-    game_title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
     review = models.CharField(max_length=450)
     reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    rating = models.FloatField(max_length=10, default=0)
+    rating = models.FloatField(max_length=10, default=0, null=True)
     likenumber = models.IntegerField(default=0)
     likedone = models.ManyToManyField(User, related_name='users_likes')
     date = models.DateTimeField(auto_now_add=True)
