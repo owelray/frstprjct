58efb6814e634f486f95652f1fbd9880451690d7
Owel <owelray@gmail.com>
2020-04-20 20:46:19 +0300

    Definition game url path

--- gamelist/urls.py
+++ gamelist/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
         path('register/', views.RegisterFormView.as_view()),
         path('login/', views.LoginFormView.as_view()),
         path('logout/', views.LogoutView.as_view()),
-        path('add/', views.AddView.as_view()),
-        path('<int:id>/definition/', views.DefiniteGame.as_view())
+        path('add/', views.AddReviewView.as_view()),
+        path('<int:pk>/', views.ReviewView.as_view()),
+        path('<int:id>/definition/', views.SearchGameView.as_view()),
+        path('<int:review_id>/definition/<int:game_id>/', views.DefiniteGameView.as_view())
 ]
