7f01a029d782a7b1a36b9a6c587386b8e0b1a371
Owel <owelray@gmail.com>
2020-05-10 18:32:10 +0300

    Extended review max_length to 1000 symbols and added additional game_id form

--- gamelist/forms.py
+++ gamelist/forms.py
@@ -5,7 +5,11 @@ class ReviewForm(forms.Form):
     title = forms.CharField(required=True, max_length=128, widget=forms.TextInput(attrs={
                                                            'placeholder': 'Enter game title',
                                                            'autocomplete': 'off', 'maxlength':'128',
-                                                           'contenteditable': 'true', 'spellcheck': 'false'}))
-    review = forms.CharField(required=True, max_length=450, widget=forms.Textarea(attrs={'autocomplete': 'off',
-                                                           'placeholder': 'Write your review (450 symbols max)',
-                                                           'maxlength': '450', 'name': 'textarea', 'id': 'textarea'}))
+                                                           'contenteditable': 'true', 'spellcheck': 'false',
+                                                            'class': 'search-input'}))
+    review = forms.CharField(required=True, max_length=1000, widget=forms.Textarea(attrs={'autocomplete': 'off',
+                                                           'placeholder': 'Write your review (1000 symbols max)',
+                                                           'maxlength': '1000', 'name': 'textarea', 'id': 'textarea'}))
+    game_api_id = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={'autocomplete': 'off',
+                                                                'class': 'hidden', 'name': 'game-id',
+                                                                'id': 'gameid', 'contenteditable': 'false'}))
--- gamelist/models.py
+++ gamelist/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class GameReview(models.Model):
     title = models.CharField(max_length=128)
-    review = models.CharField(max_length=450)
+    review = models.CharField(max_length=1000)
     reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_index=True)
     rating = models.FloatField(max_length=10, default=0, null=True)
     likenumber = models.IntegerField(default=0)
