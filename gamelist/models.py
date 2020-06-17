94ff53a35fa11edb731f05d7f8ad18c7ec054aa3
Owel <owelray@gmail.com>
2020-06-17 14:10:56 +0300

    Removed likenumber field

--- gamelist/models.py
+++ gamelist/models.py
@@ -8,7 +8,6 @@ class GameReview(models.Model):
     review = models.CharField(max_length=1000)
     reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_index=True)
     rating = models.FloatField(max_length=10, default=0, null=True)
-    likenumber = models.IntegerField(default=0)
     likedone = models.ManyToManyField(User, related_name='users_likes')
     date = models.DateTimeField(auto_now_add=True)
     game_url = models.URLField(null=True)
